8b230e8eb61a75ba1dbc50bc0f44f3a30ffaf0e6
moohammedddd <midorafat115566@gmail.com>
2025-02-01 00:10:29 +0200

    Part1Q1

--- Assignment002/Program.cs
+++ Assignment002/Program.cs
@@ -1,12 +1,36 @@
 ﻿namespace Assignment002
 {
-
+    public class Dayes
+    {
+        public DayWeek DayWeek { get; set; }
+    }
+    public enum DayWeek
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7,
+    }
     internal class Program
     {
     
         static void Main(string[] args)
         {
-            
+            #region Part01
+            #region Q1
+            int enumLength = Enum.GetValues(typeof(DayWeek)).Length;
+            Dayes Dayes = new Dayes();
+            for (int i = 1; i < enumLength; i++)
+            {
+                Dayes.DayWeek = (DayWeek)i;
+                Console.WriteLine(Dayes.DayWeek);
+            }
+            #endregion
+            #endregion
+
         }
     }
 }
